4862e39a2b1905130b15eea6fc2bbed403f33842
olive-k <olive.kingsley@student-cs.fr>
2021-02-17 15:37:03 +0100

    venv

--- scraper.py
+++ scraper.py
@@ -11,8 +11,8 @@ def run():
     sections_query = "https://en.wikipedia.org/w/api.php?action=parse&page=" + page_title + "&format=json&prop=sections"
     references_query = "https://en.wikipedia.org/w/api.php?action=parse&page=" + page_title + "&format=json&prop=externallinks"
     text_query = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&titles=" + page_title + "&format=json&exsectionformat=wiki"
-    results = []
-    result = {}
+    keys = ['Sections','Text','References','Text/Reference']
+    result = {key: None for key in keys}
 
     with urllib.request.urlopen(sections_query) as url:
         sections_data = json.loads(url.read().decode())['parse']['sections']
@@ -27,13 +27,46 @@ def run():
         text_data = text_data[next(iter(text_data))]['extract']
         print(text_data)
     
-    archived_url = "https://web.archive.org/web/20190330170311/https://www.telegraph.co.uk/travel/city-breaks/most-expensive-and-cheapest-cities-2018/"
-    http_request = requests.get(archived_url).text
-    print("get worked!!")
-    soup = BeautifulSoup(http_request, 'html.parser').findAll('div', class_ = 'component-content')
-    print(len(soup))
-    print(soup[0].prettify())
+    sections_title = ["main"]
+    sections_values = []
+    sections_value, text_data = text_data.split("== "+ sections_data[0]['line'] + " ==")
+    sections_values.append(sections_value)
+    for ind, value in enumerate(sections_data):
+        if ind == len(sections_data) - 2:
+            break
+        header = int(sections_data[ind+1]['level'])*'='
+        sections_value, text_data = text_data.split(header + " " + sections_data[ind+1]['line'] + " " + header)
+        sections_title.append(sections_data[ind]['line'])
+        sections_values.append(sections_value)
 
+    sections_count = [ i for i in range(len(sections_title)) ]
+
+    result['Sections'] = dict(zip(sections_count, sections_title))
+    result['Text'] = dict(zip(sections_count, sections_values))
+
+    archived_references_data = []
+    for data in references_data:
+        if "https://web.archive.org/web/" in data:
+            archived_references_data.append(data)
+    
+    references_data_text = []
+    for archived_url in archived_references_data:
+        http_request = requests.get(archived_url).text
+        if BeautifulSoup(http_request, 'html.parser').findAll('article'):
+            soup = BeautifulSoup(http_request, 'html.parser').findAll('article')[0].findChildren()
+            ref_text = ""
+            for tag in soup:
+                if tag.getText():
+                    ref_text = ref_text + tag.getText()
+            references_data_text.append(ref_text)
+
+    references_count = [ i for i in range(len(archived_references_data)) ]
+
+    result['References'] = dict(zip(references_count, archived_references_data))
+    result['Text/Reference'] = dict(zip(references_count, references_data_text))
+
+    with open('result.json', 'w') as fp:
+        json.dump(result, fp)
 
 if __name__ == '__main__':
     run()
