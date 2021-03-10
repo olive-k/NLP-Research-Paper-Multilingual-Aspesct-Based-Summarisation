f03b08f43a4f4e11ee8eb9fce8def66233e8e568
olive-k <olive.kingsley@student-cs.fr>
2021-03-10 15:30:30 +0100

    Finalised get_page_json

---
+++ domain_list_scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import re
+import urllib.request
+import json
+import time
+
+
+def get_domain_list(domain_url):
+    
+
+
+if __name__ == '__main__':
+    main_domain_url = 'https://en.wikipedia.org/wiki/Category:Sports'
+    get_domain_list(main_domain_url)
